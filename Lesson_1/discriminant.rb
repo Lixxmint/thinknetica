aaa0607a26b05da923c746b061a094c480913d0a
Lixxmint <the.lixxmint@gmail.com>
2018-11-10 12:46:55 +0300

    Add files via upload

--- Lesson_1/discriminant.rb
+++ Lesson_1/discriminant.rb
@@ -5,13 +5,13 @@ b = gets.chomp.to_f
 puts "Введи значение c"
 c = gets.chomp.to_f
 d = b**2 - 4 * a * c
-d_sqrt = Math.sqrt(d)
 if d < 0
   puts "Дискриминант равен #{d}. Корней нет"
 elsif d == 0
   x = (-b)/2 * a
   puts "Дискриминант равен #{d}. Корень один = #{x}"
 else
+  d_sqrt = Math.sqrt(d)
   x_1 = (-b + d_sqrt) / 2 * a
   x_2 = (-b - d_sqrt) / 2 * a
   puts "Дискриминант равен #{d}, Корни: Х1 = #{x_1}, X2 = #{x_2}"
