4e31daddc75eda7ee634f604bd88be76b186c2aa
Lixxmint <the.lixxmint@gmail.com>
2018-12-01 18:51:06 +0300

    Update train.rb

--- lesson_3/train.rb
+++ lesson_3/train.rb
@@ -1,15 +1,15 @@
 class Station
-  attr_reader :all_train
+  attr_reader :all_train, :name
 
-  def initialize(name_station)
-    @name_station = name_station
+  def initialize(name)
+    @name = name
     @all_train = []
   end
   def add_train(train)
     @all_train << train
   end
   def trains_by_type(type)
-    #@all_train.each { |train| train if train.type == type} надо разобрать
+    @all_train.select { |train| train.type == type }
   end
   def move_train(train)
     @trains.delete train
@@ -29,26 +29,25 @@ class Route
     @stations.delete(station) if @station_list.first != station && @station_list.last != station
   end
   def show_stations
-    @station_list.each_with_index do |station, index|
-      puts "#{index + 1}: #{station}"
-    end
+    @station_list.each_with_index do |station, index|  puts "#{index + 1}: #{station.name}"
   end
 end
 
 class Train
-  attr_reader :count_wagons, :speed, :type_train
+  attr_reader :count_wagons, :speed, :type
 
-  def initialize(nubmer_train, type_train, count_wagons)
-    @nubmer_train = nubmer_train
-    @type_train = type_train
+  def initialize(nubmer, type, count_wagons)
+    @nubmer = nubmer
+    @type = type
     @count_wagons = count_wagons
-    @current_route = nil
+    @route = nil
+    @speed = 0
   end
-  def speed_up
-    @speed +=10 if @speed < 150
+  def speed_up(speed)
+    @speed +=speed if @speed < 150
   end
-  def speed_down
-    @speed -=10 if @speed > 0
+  def speed_down(speed)
+    @speed -=speed if @speed > 0
   end
   def stop
     @speed = 0
@@ -57,34 +56,29 @@ class Train
     @count_wagons +=1 if @speed.zero?
   end
   def del_wagon
-      @count_wagons -=1 if @speed.zero? && @count_wagons > 0
+    @count_wagons -=1 if @speed.zero? && @count_wagons > 0
   end
   def set_route(route)
-    @current_route = route
+    @route = route
     @station_index = 0
   end
   def move_forward
     @station_index += 1 if @route.stations.size - 1 > @station_index
   end
-
   def move_backward
     @station_index -= 1 unless @station_index.zero?
   end
-
   def previous_station
     get_station_by_index @station_index - 1
   end
-
   def current_station
     get_station_by_index @station_index
   end
-
   def next_station
     get_station_by_index @station_index + 1
   end
-
   def get_station_by_index(index)
     return nil if index < 0
-
-    @route.stations[index]
+    @route.station_list[index]
   end
+end
